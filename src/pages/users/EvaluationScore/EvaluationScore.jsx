ceb3b071b9fbd633a6e0109303fe44315e4348ea
razikr <razikrehim123@gmal.com>
2025-03-22 06:42:09 +0530

    more code structuring

--- src/pages/users/EvaluationScore/EvaluationScore.jsx
+++ src/pages/users/EvaluationScore/EvaluationScore.jsx
@@ -7,6 +7,9 @@ import QuarterSelector from "./components/QuarterSelector";
 import Pending from "./assets/Pending";
 import Ongoing from "./assets/Ongoing";
 import Approval from "./assets/Approval";
+import Submitted from "./assets/Submitted";
+
+import Logo from "./assets/Logo"
 
 
 
@@ -18,6 +21,7 @@ import {
   Trophy,
   CircleEllipsis,
 } from "lucide-react";
+
 
 const data = [
   {
@@ -66,38 +70,9 @@ export default function EvaluationScore() {
       <div className="min-h-full bg-[#FCFCFC] px-6 py-4 ">
         <div className="min-w-full min-h-17 py-3 flex items-center size-11 bg-[#FFFFFF] mb-3 border border-gray-border1 rounded-[12px] shadowbox">
           <div className=" ml-3  ">
-            <svg
-              width="44"
-              height="44"
-              viewBox="0 0 44 44"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                x="0.5"
-                y="0.5"
-                width="43"
-                height="43"
-                rx="7.5"
-                fill="#FCECEA"
-              />
-              <rect
-                x="0.5"
-                y="0.5"
-                width="43"
-                height="43"
-                rx="7.5"
-                stroke="#FFCCC5"
-              />
-              <path
-                d="M21.25 28.25H19C17.9 28.25 17 29.15 17 30.25V30.5H16C15.59 30.5 15.25 30.84 15.25 31.25C15.25 31.66 15.59 32 16 32H28C28.41 32 28.75 31.66 28.75 31.25C28.75 30.84 28.41 30.5 28 30.5H27V30.25C27 29.15 26.1 28.25 25 28.25H22.75V25.96C22.5 25.99 22.25 26 22 26C21.75 26 21.5 25.99 21.25 25.96V28.25Z"
-                fill="#E14026"
-              />
-              <path
-                d="M28.48 21.64C29.14 21.39 29.72 20.98 30.18 20.52C31.11 19.49 31.72 18.26 31.72 16.82C31.72 15.38 30.59 14.25 29.15 14.25H28.59C27.94 12.92 26.58 12 25 12H19C17.42 12 16.06 12.92 15.41 14.25H14.85C13.41 14.25 12.28 15.38 12.28 16.82C12.28 18.26 12.89 19.49 13.82 20.52C14.28 20.98 14.86 21.39 15.52 21.64C16.56 24.2 19.06 26 22 26C24.94 26 27.44 24.2 28.48 21.64ZM24.84 18.45L24.22 19.21C24.12 19.32 24.05 19.54 24.06 19.69L24.12 20.67C24.16 21.27 23.73 21.58 23.17 21.36L22.26 21C22.12 20.95 21.88 20.95 21.74 21L20.83 21.36C20.27 21.58 19.84 21.27 19.88 20.67L19.94 19.69C19.95 19.54 19.88 19.32 19.78 19.21L19.16 18.45C18.77 17.99 18.94 17.48 19.52 17.33L20.47 17.09C20.62 17.05 20.8 16.91 20.88 16.78L21.41 15.96C21.74 15.45 22.26 15.45 22.59 15.96L23.12 16.78C23.2 16.91 23.38 17.05 23.53 17.09L24.48 17.33C25.06 17.48 25.23 17.99 24.84 18.45Z"
-                fill="#E14026"
-              />
-            </svg>
+
+            <Logo/>
+            
           </div>
           <div className="flex flex-col gap-1">
             <h1 className="ml-4  text-lg/6 align-text-top font-medium ">
@@ -113,7 +88,7 @@ export default function EvaluationScore() {
             <Searchcomponent />
           </div>
 
-          <div className="flex flex-wrap gap-1 sm:gap-x-4 justify-center items-center ">
+          <div className="flex flex-wrap gap-1 sm:gap-4 justify-center items-center ">
             <Dropdown text="Auditor" />
             <Dropdown text="Region" />
             <Dropdown text="Type" />
@@ -123,36 +98,14 @@ export default function EvaluationScore() {
             <div className="ml-auto  max-h-7 mb-4 sm:mb-0  font-inter py-1.5 flex flex-wrap flex-shrink justify-center  items-center  sm:gap-2 ">
               <Pending/>
               <h2 className="text-text-gray text-xs  font-normal">Pending</h2>
-              <h2 classname="text-sm font-normaln">12</h2>
-
-             
+              <h2 classname="text-sm font-normal">12</h2>
               <Ongoing/>
               <h2 className="text-text-gray text-xs">Ongoing</h2>
               <h2 classname="text-sm">12</h2>
-
              <Approval/>
-
               <h2 className="text-text-gray text-xs">Approval</h2>
               <h2 classname="text-sm">12</h2>
-
-              <svg
-                width="28"
-                height="28"
-                viewBox="0 0 28 28"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  opacity="0.4"
-                  d="M14 25.6667C20.4434 25.6667 25.6667 20.4433 25.6667 14C25.6667 7.55669 20.4434 2.33334 14 2.33334C7.55672 2.33334 2.33337 7.55669 2.33337 14C2.33337 20.4433 7.55672 25.6667 14 25.6667Z"
-                  fill="#3F9A26"
-                />
-                <path
-                  d="M12.3433 18.1767C12.11 18.1767 11.8883 18.0833 11.725 17.92L8.42331 14.6183C8.08497 14.28 8.08497 13.72 8.42331 13.3817C8.76164 13.0433 9.32164 13.0433 9.65997 13.3817L12.3433 16.065L18.34 10.0683C18.6783 9.72999 19.2383 9.72999 19.5766 10.0683C19.915 10.4067 19.915 10.9667 19.5766 11.305L12.9616 17.92C12.7983 18.0833 12.5766 18.1767 12.3433 18.1767Z"
-                  fill="#3F9A26"
-                />
-              </svg>
-
+             <Submitted/>
               <h2 className="text-text-gray text-xs">Submitted</h2>
               <h2 classname="">12</h2>
             </div>
